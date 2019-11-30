dbfcd57c1386ec51007f16aa346cc4d9e2b165ae
James <Icedshot@gmail.com>
2019-11-30 19:17:38 +0000

    updating dependencies

--- render_window.cpp
+++ render_window.cpp
@@ -7,7 +7,76 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-render_window::render_window(vec2i dim, const std::string& window_title)
+void glfw_error_callback(int error, const char* description)
 {
+    fprintf(stderr, "Glfw Error %d: %s\n", error, description);
+}
 
+render_window::render_window(vec2i dim, const std::string& window_title, window_flags::window_flags flags)
+{
+    glfwSetErrorCallback(glfw_error_callback);
+
+    if(!glfwInit())
+        throw std::runtime_error("Could not init glfw");
+
+    const char* glsl_version = "#version 130";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+
+    if(flags & window_flags::DOUBLE_BUFFER)
+        glfwWindowHint( GLFW_DOUBLEBUFFER, GL_FALSE );
+
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+    //glfwWindowHint(GLFW_SAMPLES, 8);
+
+    if(flags & window_flags::SRGB)
+        glfwWindowHint(GLFW_SRGB_CAPABLE, GLFW_TRUE);
+
+    window = glfwCreateWindow(dim.x(), dim.y(), window_title.c_str(), nullptr, nullptr);
+
+    if(window == nullptr)
+        throw std::runtime_error("Nullptr window in glfw");
+
+    glfwMakeContextCurrent(window);
+
+    if(glewInit() != GLEW_OK)
+        throw std::runtime_error("Bad Glew");
+
+
+    ImGui::CreateContext(&atlas);
+
+    printf("ImGui create context\n");
+
+    ImGuiIO& io = ImGui::GetIO();
+
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+
+    if(flags & window_flags::VIEWPORTS)
+        io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+
+    ImGuiStyle& style = ImGui::GetStyle();
+
+    style.FrameRounding = 0;
+    style.WindowRounding = 0;
+    style.ChildRounding = 0;
+    style.ChildBorderSize = 0;
+    style.FrameBorderSize = 0;
+    style.WindowBorderSize = 1;
+
+    if(io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+    {
+        style.WindowRounding = 0.0f;
+        style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+    }
+
+    if(flags & window_flags::SRGB)
+        ImGui::SetStyleLinearColor(true);
+
+    io.Fonts->Clear();
+    io.Fonts->AddFontDefault();
+    ImGuiFreeType::BuildFontAtlas(&atlas, 0, 1);
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
 }
--- render_window.hpp
+++ render_window.hpp
@@ -2,16 +2,29 @@
 #define RENDER_WINDOW_HPP_INCLUDED
 
 #include <vec/vec.hpp>
+#include <imgui/imgui.h>
 
 struct GLFWwindow;
 
+namespace window_flags
+{
+    enum window_flags
+    {
+        NONE = 0,
+        SRGB = 1,
+        DOUBLE_BUFFER = 2,
+        VIEWPORTS = 4,
+    };
+}
+
 struct render_window
 {
-    render_window(vec2i dim, const std::string& window_title);
+    render_window(vec2i dim, const std::string& window_title, window_flags::window_flags flags = window_flags::NONE);
 
     vec2i get_window_size();
 
     GLFWwindow* window = nullptr;
+    ImFontAtlas atlas = {};
 };
 
 #endif // RENDER_WINDOW_HPP_INCLUDED
