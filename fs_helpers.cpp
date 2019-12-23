ae2398c334ccea1981404d8fd691c04c6bd4ad79
James <Icedshot@gmail.com>
2019-12-23 18:58:43 +0000

    untested emscriptable fs

---
+++ fs_helpers.cpp
@@ -0,0 +1,169 @@
+#include "fs_helpers.hpp"
+
+#include <fstream>
+#include "clock.hpp"
+
+#ifdef __WIN32__
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#else
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#endif // __WIN32__
+
+std::string file::read(const std::string& file)
+{
+    #ifndef __EMSCRIPTEN__
+    FILE* f = fopen(file.c_str(), "rb");
+    #else
+    FILE* f = fopen(("web/" + file).c_str(), "rb");
+    #endif
+
+    fseek(f, 0, SEEK_END);
+    long fsize = ftell(f);
+    fseek(f, 0, SEEK_SET);
+
+    std::string buffer;
+    buffer.resize(fsize + 1);
+    fread(&buffer[0], fsize, 1, f);
+    fclose(f);
+
+    return buffer;
+}
+
+void file::write(const std::string& file, const std::string& data)
+{
+    #ifndef __EMSCRIPTEN__
+    std::ofstream out(file, std::ios::binary);
+    #else
+    std::ofstream out("web/" + file, std::ios::binary);
+    #endif
+    out << data;
+}
+
+void file::write_atomic(const std::string& in_file, const std::string& data)
+{
+    if(data.size() == 0)
+        return;
+
+    #ifndef __EMSCRIPTEN__
+    std::string file = in_file;
+    #else
+    std::string file = "web/" + in_file;
+    #endif
+
+    std::string atomic_extension = ".atom";
+    std::string atomic_file = file + atomic_extension;
+    std::string backup_file = file + ".back";
+
+    #ifdef __WIN32__
+    HANDLE handle = CreateFile(atomic_file.c_str(), GENERIC_WRITE, 0, nullptr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
+    WriteFile(handle, &data[0], data.size(), nullptr, nullptr);
+    FlushFileBuffers(handle);
+    CloseHandle(handle);
+    #else
+    int fd = open(atomic_file.c_str(), O_CREAT | O_DIRECT | O_SYNC | O_TRUNC | O_WRONLY, 0777);
+
+    int written = 0;
+
+    while(written < data.size())
+    {
+        int rval = write(fd, &data[written], (int)data.size() - written);
+
+        if(rval == -1)
+        {
+            close(fd);
+            throw std::runtime_error("Errno in atomic write " + std::to_string(errno));
+        }
+
+        written += rval;
+    }
+
+    close(fd);
+
+    #endif // __WIN32__
+
+    if(!file::exists(file))
+    {
+        rename(atomic_file.c_str(), file.c_str());
+        return;
+    }
+
+    steady_timer timer;
+
+    bool write_success = false;
+    bool any_errors = false;
+
+    do
+    {
+        #ifdef __WIN32__
+        bool err = ReplaceFileA(file.c_str(), atomic_file.c_str(), backup_file.c_str(), REPLACEFILE_IGNORE_MERGE_ERRORS, nullptr, nullptr) == 0;
+        #else
+        bool err = rename(atomic_file.c_str(), file.c_str()) != 0;
+        #endif // __WIN32__
+
+        //bool err = ReplaceFileA(file.c_str(), atomic_file.c_str(), nullptr, REPLACEFILE_IGNORE_MERGE_ERRORS, nullptr, nullptr) == 0;
+
+        if(!err)
+        {
+            write_success = true;
+            break;
+        }
+
+        if(err)
+        {
+            #ifdef __WIN32__
+            printf("atomic write error %lu ", GetLastError());
+            #else
+            printf("atomic write error %i %s\n", errno, file.c_str());
+            #endif // __WIN32__
+
+            any_errors = true;
+        }
+    }
+    while(timer.get_elapsed_time_s() < 1);
+
+    if(!write_success)
+    {
+        throw std::runtime_error("Explod in atomic write");
+    }
+
+    if(any_errors)
+    {
+        printf("atomic_write had errors but recovered");
+    }
+}
+
+bool file::exists(const std::string& name)
+{
+    #ifndef __EMSCRIPTEN__
+    std::ifstream f(name.c_str());
+    #else
+    std::ifstream f(("web/" + name).c_str());
+    #endif
+    return f.good();
+}
+
+#ifdef __EMSCRIPTEN__
+struct em_helper
+{
+    em_helper()
+    {
+        #ifdef __EMSCRIPTEN__
+        EM_ASM(void, handle_mounting, (),
+        {
+            FS.mkdir('/web');
+            FS.mount(IDBFS, {}, "/web");
+        });
+        #endif // __EMSCRIPTEN__
+    }
+};
+
+namespace
+{
+    em_helper help;
+}
+
+#endif
---
+++ fs_helpers.hpp
@@ -0,0 +1,15 @@
+#ifndef FS_HELPERS_HPP_INCLUDED
+#define FS_HELPERS_HPP_INCLUDED
+
+#include <string>
+
+///a directory should be without prefixes, eg a/hello.txt
+namespace file
+{
+    std::string read(const std::string& file);
+    void write(const std::string& file, const std::string& data);
+    void write_atomic(const std::string& file, const std::string& data);
+    bool exists(const std::string& name);
+}
+
+#endif // FS_HELPERS_HPP_INCLUDED
