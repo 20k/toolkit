10240e8465f2ae3060093c7fb72f8def17ba356f
James <Icedshot@gmail.com>
2019-12-26 21:58:13 +0000

    working clipboard set

--- clipboard.cpp
+++ clipboard.cpp
@@ -7,10 +7,12 @@
 #endif // __EMSCRIPTEN__
 
 #ifdef __EMSCRIPTEN__
-EM_JS(void, copy_js, (),
+EM_JS(void, copy_js, (const char* data),
 {
+    var set_name = UTF8ToString(data);
+
     Module.clipbuffer.focus();
-    Module.clipbuffer.value = "hellothere";
+    Module.clipbuffer.value = set_name;
     Module.clipbuffer.setSelectionRange(0, Module.clipbuffer.value.length);
     var succeeded = document.execCommand('copy');
 });
@@ -21,6 +23,6 @@ void clipboard::set(const std::string& data)
     #ifndef __EMSCRIPTEN__
     glfwSetClipboardString(NULL, data.c_str());
     #else
-    copy_js();
+    copy_js(data.c_str());
     #endif
 }
